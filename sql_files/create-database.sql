40a9ec45f29ca54c9a5d20fb82b400d2b2a802d5
JaredDyreson <jared.dyreson@gmail.com>
2020-10-28 13:41:39 -0700

    done for the day

--- sql_files/create-database.sql
+++ sql_files/create-database.sql
@@ -23,6 +23,7 @@ CREATE TABLE course (
     title varchar(255) NOT NULL,
     textbook varchar(255) NOT NULL,
     units int(10) NOT NULL,
+    supplier varchar(255) NOT NULL, -- the department
     -- Not sure how to make a list of pre-reqs for SQL specifically
     PRIMARY KEY(identifier)
 );
@@ -54,18 +55,25 @@ CREATE TABLE student (
 
 
 
--- Figure out how to fill in the data
+-- 3 Professors
 
 INSERT INTO professor(ssn, name, address, telephone, city, state, zipcode) VALUES (123456789, "Joe Biden", "123 First Street", "123-456-7891", "Fullerton", "CA", 92831);
 INSERT INTO professor(ssn, name, address, telephone, city, state, zipcode) VALUES (987654321, "Donald Trump", "1600 Pennsylvania Ave.", "903-213-1213" ,"NW Washington, DC", "DC",20500);
 INSERT INTO professor(ssn, name, address, telephone, city, state, zipcode) VALUES (234211423, "Joe Rogan", "345 Second Avenue", "213-132-1231", "Austin", "Texas",73301);
 
+-- 2 Departments
 
 INSERT INTO department(identifier, name, telephone, location, chairperson) VALUES (12112, "Computer Science", "657-278-3700", "Department of Computer Science (CS-522) California State University, Fullerton 800 N. State College Blvd. Fullerton, CA 92834", "Joe Rogan");
 INSERT INTO department(identifier, name, telephone, location, chairperson) VALUES (21314, "English", "657-278-3163", "California State University, Fullerton Gordon Hall 322 Fullerton, California 92831" , "Joe Biden");
 
+-- 4 Courses (2 English, 2 CS)
 
--- Randomly generated based on Python script
+INSERT INTO course(identifier, title, textbook, units, supplier) VALUES (123456, "CS-131 : Introduction to Datastructures", "Data Structures and Algorithms in C++", 4, "Computer Science");
+INSERT INTO course(identifier, title, textbook, units, supplier) VALUES (324224, "CS-121 : Introduction to OOP", "Classes and Structures in C++", 3, "Computer Science");
+INSERT INTO course(identifier, title, textbook, units, supplier) VALUES (211314, "ENG-100 : Reading and Writing", "How to Formulate an Argument", 3, "English");
+INSERT INTO course(identifier, title, textbook, units, supplier) VALUES (213411, "ENG-123 : Shakespearean Literature", "Hamlet", 3, "English");
+
+-- Randomly generated students based on Python script
 
 INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(53969038656, "William", "Dyreson", "123 Main Street", "935-0665368");
 INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(71481021819, "Casey", "Ramirez", "123 Main Street", "142-1701891");
@@ -74,3 +82,6 @@ INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(36590004440, 
 INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(98207081500, "Jared", "Ramirez", "123 Main Street", "128-7009850");
 INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(65628213994, "Casey", "Smith", "123 Main Street", "422-1569836");
 INSERT INTO student(cwid, fname, lname, address, telephone) VALUES(18954666233, "William", "McCarthy", "123 Main Street", "926-3146835");
+
+
+-- Randomly generated student records based on Python script
