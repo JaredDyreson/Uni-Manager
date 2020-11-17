cae9ed9ba7cc1ceac70c7b0dcee449d9f609347b
JaredDyreson <jared.dyreson@gmail.com>
2020-11-17 12:05:48 -0800

    PHP Classes for each element in the ER Diagram

--- includes/classes/Department.php
+++ includes/classes/Department.php
@@ -1,6 +1,12 @@
 <?php
+
+include_once("Geolocation.php");
+include_once("Telephone.php");
+include_once("Professor.php");
+
+
 class Department {
-    public $identifier;
+    public $identifier; // Primary Key
     public $name;
     public $telephone;
     public $location;
@@ -12,4 +18,43 @@ class Department {
     }
 }
 
+/*
+* Driver code
+*/
+
+//$professor_geolocation_data = array(
+    //'123 First Avenue',
+    //'Fullerton',
+    //'CA',
+    //'92831'
+//);
+
+//$department_geolocation_data = array(
+    //"800 N State College Blvd",
+    //"Fullerton",
+    //"CA",
+    //'92831'
+//);
+
+//$professor_information = array(
+   //new Geolocation($professor_geolocation_data),
+   //'Jared Dyreson', 
+   //'M',
+   //450000,
+   //array('Computer Science', 'Underwater Basket Weaving'),
+   //'Professor',
+   //'000-00-00', 
+   //new Telephone("123-456-7890")
+//);
+
+//$departmentInformation = array(
+    //10,
+    //"Computer Science",
+    //new Telephone("789-432-2121"),
+    //new Geolocation($department_geolocation_data),
+    //new Professor($professor_information)
+//);
+
+//$computerScienceDepartment = new Department($departmentInformation);
+//echo $computerScienceDepartment->location->address;
 ?>
--- includes/classes/Professor.php
+++ includes/classes/Professor.php
@@ -1,26 +1,44 @@
 <?php
-include_once('Department.php');
+include_once('Geolocation.php');
+include_once('Telephone.php');
 
 class Professor {
-    public $ssn;
+    public $geolocation;
     public $name;
-    public $address;
+    public $sex;
+    public $salary;
+    public $position;
+    public $degrees;
+    public $ssn; // Primary Key
     public $telephone;
-    public $city;
-    public $state;
-    public $zipcode;
 
     function __construct($container_){
-        list($this->ssn, $this->name, $this->address, $this->telephone, $this->city, $this->state, $this->zipcode) = $container_;
+        list($this->geolocation, $this->name, $this->sex, $this->salary, $this->degrees, $this->position, $this->ssn, $this->telephone) = $container_;
     }
 }
 
-$professor_information = array('coffee', 'brown', 'caffeine', 'brown', 'brown', 'brown', 'brown');
+/*
+* This is example driver code
+*/
 
-$professor = new Professor($professor_information);
-$department_information  = array('coffee', 'cheese', 'milk', 'cookies', $professor);
+//$professor_geolocation_data = array(
+    //'123 First Avenue',
+    //'Fullerton',
+    //'CA',
+    //'92831'
+//);
 
-$dep = new Department($department_information);
-echo $professor->ssn;
+//$professor_information = array(
+   //new Geolocation($professor_geolocation_data),
+   //'Jared Dyreson', 
+   //'M',
+   //450000,
+   //array('Computer Science', 'Underwater Basket Weaving'),
+   //'Professor',
+   //'000-00-00', 
+   //new Telephone("123-456-7890")
+//);
+
+//$professor = new Professor($professor_information);
 
 ?>
