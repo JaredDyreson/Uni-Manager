6a7a3fb68aaddc904a580f18426758f3112584bc
JaredDyreson <jared.dyreson@gmail.com>
2020-11-30 15:15:25 -0800

    added two more professors and ensured it worked

--- sql_files/fill-structure.sql
+++ sql_files/fill-structure.sql
@@ -1,10 +1,11 @@
 -- Professors
 
 -- Jared Dyreson
-insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Jared Dyreson", "M", 45000, "Professor of Computer Science", "Computer Science, Computer Engineering", 123456781, "714-681-6918");
+insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Jared Dyreson", "M", 45000, "Professor of Computer Science", "Computer Science, Computer Engineering", 123456781, "714-681-6917");
 
 -- Katherine Correra
--- insert into professor(ssn, name, address, telephone) values (123456782, "Katherine Correra", "800 N State College Blvd, Fullerton, CA 92831", "(714) 681-6918");
+insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Katherine Correra", "F", 55000, "Professor of Computer Science", "Computer Science, English", 123456782, "714-681-6918");
 
 -- Kevin Durate
--- insert into professor(ssn, name, address, telephone) values (123456783, "Kevin Durate", "800 N State College Blvd, Fullerton, CA 92831", "(714) 681-6919");
+insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Kevin Durate", "M", 90000, "Professor of Computer Science", "Computer Science, Mechanical Engineering", 123456783, "714-681-6919");
+
