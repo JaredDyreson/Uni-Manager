1e478572de86c065faaab307ed3fecb8f7fca55b
Kevin Duarte <kevinduarte2007@gmail.com>
2020-12-02 20:45:33 -0800

    Added Fill Structure for Student Table

--- sql_files/fill-structure.sql
+++ sql_files/fill-structure.sql
@@ -7,5 +7,16 @@ insert into professor(address, name, sex, salary, position, degrees, ssn, teleph
 insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Katherine Correra", "F", 55000, "Professor of Computer Science", "Computer Science, English", 123456782, "714-681-6918");
 
 -- Kevin Durate
-insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Kevin Durate", "M", 90000, "Professor of Computer Science", "Computer Science, Mechanical Engineering", 123456783, "714-681-6919");
+insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Kevin Duarte", "M", 90000, "Professor of Computer Science", "Computer Science, Mechanical Engineering", 123456783, "714-681-6919");
 
+
+-- Students
+
+-- Kevin Duarte
+insert into student(firstN, lastN, cwid, address, telephone) values ("Kevin", "Duarte", 888888888, "800 N State College Blvd, Fullerton, CA 92831", "714-681-6919");
+
+-- Katherine Correra
+insert into student(firstN, lastN, cwid, address, telephone) values ("Katherine", "Correra", 888888887, "800 N State College Blvd, Fullerton, CA 92831", "714-681-6918");
+
+-- Jared Dyreson
+insert into student(firstN, lastN, cwid, address, telephone) values ("Jared", "Dyreson", 888888886, "800 N State College Blvd, Fullerton, CA 92831", "714-681-6917");
