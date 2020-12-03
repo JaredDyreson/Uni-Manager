5c5ec7aa3a872e113237e94d61806cd80c0158f4
Kevin Duarte <kevinduarte2007@gmail.com>
2020-12-02 20:51:06 -0800

    Added Fill Structure for Department Table

--- sql_files/fill-structure.sql
+++ sql_files/fill-structure.sql
@@ -6,7 +6,7 @@ insert into professor(address, name, sex, salary, position, degrees, ssn, teleph
 -- Katherine Correra
 insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Katherine Correra", "F", 55000, "Professor of Computer Science", "Computer Science, English", 123456782, "714-681-6918");
 
--- Kevin Durate
+-- Kevin Duarte
 insert into professor(address, name, sex, salary, position, degrees, ssn, telephone) values ("800 N State College Blvd, Fullerton, CA 92831", "Kevin Duarte", "M", 90000, "Professor of Computer Science", "Computer Science, Mechanical Engineering", 123456783, "714-681-6919");
 
 
@@ -20,3 +20,14 @@ insert into student(firstN, lastN, cwid, address, telephone) values ("Katherine"
 
 -- Jared Dyreson
 insert into student(firstN, lastN, cwid, address, telephone) values ("Jared", "Dyreson", 888888886, "800 N State College Blvd, Fullerton, CA 92831", "714-681-6917");
+
+-- Departments
+
+-- Computer Science
+insert into department(name, number_, location, telephone) values ("Computer Science", "1", "West Nutmeg Building", "909-896-2753");
+
+-- Engineering 
+insert into department(name, number_, location, telephone) values ("Engineering", "2", "North Copperston Building", "909-896-7878");
+
+-- Mathematics 
+insert into department(name, number_, location, telephone) values ("Mathematics", "3", "Junior Hall", "909-896-8512");
